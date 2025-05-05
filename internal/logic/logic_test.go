fceb14ff9f5da1748bd0e30563a3aa7131f0b8e7
Osmar Gonzalez <osmarlenin.gonzalezescorche@externos-cl.cencosud.com>
2025-05-05 06:58:32 -0300

    refactor: Enhance test organization and add new test cases
    Reorganized test suite by renaming the package, improving structure, and migrating to `testify` for assertions. Added table-driven tests for `Sum` and `CheckEmail` functions and included benchmarks for performance evaluation. Cleaned up legacy code for better readability and maintainability.

--- internal/logic/logic_test.go
+++ internal/logic/logic_test.go
@@ -1,32 +1,121 @@
-package logic
+package logic_test
 
-import "testing"
+import (
+	"fmt"
+	"github.com/og11423074s/go-test-basic/internal/logic"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	fmt.Println("Running tests")
+	exitCode := m.Run()
+	fmt.Println("Exiting with code", exitCode)
+	os.Exit(exitCode)
+}
 
 func TestSum(t *testing.T) {
-	total := Sum(1, 2, 3, 4, 5)
+	got := logic.Sum(1, 2, 3, 4, 5)
 	expected := 15
-	if total != expected {
-		t.Errorf("Expected 15, got %d", total)
-	}
 
-	result := internalSum(4, 5)
+	assert.Equal(t, expected, got)
+
+	got = logic.Sum(4, 5)
 	expected = 9
-	if result != expected {
-		t.Errorf("Expected 9, got %d", result)
-	}
+
+	assert.Equal(t, expected, got)
+
 }
 
 func TestCheckEmail(t *testing.T) {
-	result := CheckEmail("test@test.com")
+	got := logic.CheckEmail("test@test.com")
 	expected := true
-	if result != expected {
-		t.Errorf("CheckEmail expected %t, got %t", expected, result)
-	}
+	assert.True(t, got)
 
 	emailInvalid := "testtest.com.ar"
-	result = CheckEmail(emailInvalid)
+	got = logic.CheckEmail(emailInvalid)
 	expected = false
-	if result != expected {
-		t.Errorf("CheckEmail expected %t, got %t", expected, result)
+	assert.False(t, expected, got)
+}
+
+func TestSumMatrix(t *testing.T) {
+	cases := []struct {
+		name     string
+		input    []int
+		expected int
+	}{
+		{
+			name:     "Sum of 1, 2 and 3",
+			input:    []int{1, 2, 3},
+			expected: 6,
+		},
+		{
+			name:     "Sum of 1, 2, 3 and 4",
+			input:    []int{1, 2, 3, 4},
+			expected: 10,
+		},
+		{
+			name:     "Sum of 1, 2, 3, 4 and 5",
+			input:    []int{1, 2, 3, 4, 5},
+			expected: 15,
+		},
+		{
+			name:     "Sum of empty",
+			input:    []int{},
+			expected: 0,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := logic.Sum(c.input...)
+			assert.Equal(t, c.expected, got)
+		})
+
+	}
+}
+
+func TestCheckEmailMatrix(t *testing.T) {
+	cases := []struct {
+		name     string
+		input    string
+		expected bool
+	}{
+		{
+			name:     "Valid email",
+			input:    "test@gmail.com",
+			expected: true,
+		},
+		{
+			name:     "Invalid email",
+			input:    "test@gmail",
+			expected: false,
+		},
+		{
+			name:     "Empty email",
+			input:    "",
+			expected: false,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			got := logic.CheckEmail(c.input)
+			assert.Equal(t, c.expected, got)
+		})
+	}
+
+}
+
+func BenchmarkSum(b *testing.B) {
+	for b.Loop() {
+		_ = logic.Sum(1, 2, 3, 4, 5)
+	}
+}
+
+func BenchmarkCheckEmail(b *testing.B) {
+	for b.Loop() {
+		_ = logic.CheckEmail("test@gmail.com")
 	}
 }
